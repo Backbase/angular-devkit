438706341eba4a072358a9039d5779b21c7111db
David Scourfield <davids@backbase.com>
2020-06-29 19:09:48 +0100

    EA-536 Initial project setup, with build and test scripts

---
+++ gulpfile.js
@@ -0,0 +1,17 @@
+const gulp = require('gulp');
+const ts = require('gulp-typescript');
+const merge = require('merge2');  // Requires separate installation
+
+gulp.task('default', function() {
+  const tsProject = ts.createProject('tsconfig.lib.json');
+  const dest = tsProject.options.outDir;
+  const tsResult = tsProject.src()
+    .pipe(tsProject());
+
+  return merge([
+    tsResult.dts.pipe(gulp.dest(dest)),
+    tsResult.js.pipe(gulp.dest(dest)),
+    gulp.src('./src/**/*.json').pipe(gulp.dest(dest)),
+    gulp.src('./package.json').pipe(gulp.dest(dest)),
+  ]);
+});
---
+++ src/builders/cx-package/cx-package.spec.ts
@@ -0,0 +1,5 @@
+describe('cx-package builder', () => {
+  it('should be truthy', () => {
+    expect(true).toBeTruthy();
+  });
+});
