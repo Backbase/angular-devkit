bb993520f2aa2f7a204151616139e6dd7aa2bc04
David Scourfield <davids@backbase.com>
2020-07-03 10:29:07 +0100

    EA-536 Use jsdom instead of xml2js for manipulating the model.xml file

--- src/builders/cx-package/item-builders/page-builder.ts
+++ src/builders/cx-package/item-builders/page-builder.ts
@@ -2,7 +2,7 @@ import { CxPackageBuilderOptionsItem } from '../types';
 import { BuilderContext } from '@angular-devkit/architect';
 import * as path from 'path';
 import * as fs from 'fs';
-import * as xml2js from 'xml2js';
+import { JSDOM } from 'jsdom';
 import { promisify } from 'util';
 
 const ncp = promisify(require('ncp'));
@@ -61,7 +61,7 @@ export async function createPageContent(
   }
 
   async function createModelXml(entryFileName, iconFileName) {
-    const modelXml: any = await readModelXml();
+    const modelXml: JSDOM = await readModelXml();
 
     const properties = getPageProperties(modelXml);
 
@@ -71,68 +71,65 @@ export async function createPageContent(
     await writeModelXml(modelXml);
   }
 
-  async function readModelXml() {
-    const parser = new xml2js.Parser();
+  async function readModelXml(): Promise<JSDOM> {
     const modelXml = path.resolve(context.workspaceRoot, item.modelXml);
-    return new Promise((resolve, reject) => {
-      fs.readFile(modelXml, function (err, data) {
-        if (err) {
-          return reject(err);
-        }
-        parser.parseString(data, function (err, result) {
-          if (err) {
-            return reject(err);
-          }
-          resolve(result);
-        });
-      });
+    return JSDOM.fromFile(modelXml, {
+      contentType: 'text/xml',
     });
   }
 
-  async function writeModelXml(modelXmlJson) {
-    const builder = new xml2js.Builder();
-    const xml = builder.buildObject(modelXmlJson);
+  async function writeModelXml(modelXml: JSDOM) {
     const dest = path.resolve(itemZipContentsDir, 'model.xml');
-    await fs.promises.writeFile(dest, xml, 'utf8');
+    await fs.promises.writeFile(dest, modelXml.serialize(), 'utf8');
   }
 }
 
-function getPageProperties(modelXml) {
-  const page = modelXml?.catalog?.page?.[0];
-  if (!page) {
+function getPageProperties(modelXml: JSDOM) {
+  const catalog = modelXml.window.document.documentElement;
+  if (catalog.tagName !== 'catalog') {
     throw new Error(
-      'Invalid model.xml - expected path modelXml.catalog.page to be present'
+      `Invalid model.xml - expected document element to have tag name 'catalog', but was: '${catalog.tagName}'`
     );
   }
 
-  if (!page.properties) {
-    page.properties = [
-      {
-        property: [],
-      },
-    ];
+  const page: any = Array.from(catalog.children).find(
+    (elem: any) => elem.tagName === 'page'
+  );
+  if (!page) {
+    throw new Error(
+      'Invalid model.xml - expected a <page> child of the <catalog> document element'
+    );
   }
 
-  return page.properties;
+  let properties = Array.from(page.children).find(
+    (elem: any) => elem.tagName === 'properties'
+  );
+
+  if (!properties) {
+    properties = modelXml.window.document.createElement('properties');
+    page.appendChild(properties);
+  }
+
+  return properties;
 }
 
 function setProperty(properties, name, value) {
-  const prop = properties[0].property.find((p) => p['$']?.name === name);
-  if (prop) {
-    prop.value[0]._ = value;
-  } else {
-    properties[0].property.push({
-      $: {
-        name: name,
-      },
-      value: [
-        {
-          _: value,
-          $: {
-            type: 'string',
-          },
-        },
-      ],
-    });
+  let propertyElem: any = Array.from(properties.children).find(
+    (elem: any) =>
+      elem.tagName === 'property' && elem.getAttribute('name') === name
+  );
+  if (!propertyElem) {
+    propertyElem = properties.ownerDocument.createElement('property');
+    propertyElem.setAttribute('name', name);
+    properties.appendChild(propertyElem);
   }
+  let valueElem: any = Array.from(propertyElem.children).find(
+    (elem: any) => elem.tagName === 'value'
+  );
+  if (!valueElem) {
+    valueElem = propertyElem.ownerDocument.createElement('value');
+    propertyElem.appendChild(valueElem);
+  }
+  valueElem.setAttribute('type', 'string');
+  valueElem.textContent = value;
 }
