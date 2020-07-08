3cbadbb1d007042fd506c780dc350254f6a018c9
David Scourfield <davids@backbase.com>
2020-07-08 16:32:10 +0100

    Move generation of source files to gulpfile

--- gulpfile.js
+++ gulpfile.js
@@ -2,10 +2,22 @@ const gulp = require('gulp');
 const ts = require('gulp-typescript');
 const merge = require('merge2');
 const path = require('path');
+const { promisify } = require('util');
+const rimraf = promisify(require('rimraf'));
+const exec = require('child_process').exec;
 
-gulp.task('default', function() {
+function generateSources(cb) {
+  exec('json2ts -i src/builders/cx-package/schema.json -o src/builders/cx-package/schema.d.ts', (err, stdout, stderr) => {
+    stdout && console.log(stdout);
+    stderr && console.error(stderr);
+    cb(err);
+  });
+}
+
+async function build() {
   const tsProject = ts.createProject('tsconfig.lib.json');
   const dest = tsProject.options.outDir;
+  await rimraf(dest);
   const tsResult = tsProject.src()
     .pipe(tsProject());
 
@@ -15,4 +27,7 @@ gulp.task('default', function() {
     gulp.src('./src/**/*.json').pipe(gulp.dest(path.resolve(dest, 'src'))),
     gulp.src('./package.json').pipe(gulp.dest(dest)),
   ]);
-});
+}
+
+exports.generateSources = generateSources;
+exports.default = gulp.series(generateSources, build);
